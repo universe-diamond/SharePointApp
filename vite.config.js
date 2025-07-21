4fb739a9050bd1794b15c2242a7879889964ae5e
Jorge Ramos <developefullstack420@gmail.com>
2025-07-21 11:57:00 -0400

    update the deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2022'
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
