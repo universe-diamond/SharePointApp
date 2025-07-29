0d741852ba0649bf7209a8c53ef6b55586fd5dc4
Diamond <devstar0428@gmail.com>
2025-07-29 05:15:30 -0400

    [fix-7/28-3] Dashboard / Members workload | replaced with real project status

--- src/components/Cards/MemberWorkloadCard.vue
+++ src/components/Cards/MemberWorkloadCard.vue
@@ -1,14 +1,14 @@
 <script setup>
-	import MemberWorkloadChart from '../Charts/MemberWorkload.vue'
+import MemberWorkloadChart from "../Charts/MemberWorkload.vue";
 
-	const props = defineProps({
-		member: Array
-	})
-
+const props = defineProps({
+  member: Array,
+  status: Array,
+});
 </script>
 
 <template>
-	<div style="min-height: 625px;">
-		<MemberWorkloadChart :memberList="member"/>
-	</div>
+  <div style="min-height: 625px">
+    <MemberWorkloadChart :memberList="member" :statusList="status" />
+  </div>
 </template>
